c2d8df759a61ab48783f7c1db63fd522f04dd3c9
zat-is-me <taahm1@morgan.edu>
2021-04-06 15:09:28 -0500

    remove comment

--- BinaryReversal.java
+++ BinaryReversal.java
@@ -2,33 +2,33 @@
 public class BinaryReversal {
     public static void main(String[] args) {
         String decimal = String.valueOf (47);
-        System.out.println (binaryReversal (decimal));
+        System.out.println (binaryReversal (decimal));                              // Output
     }
     private static String binaryReversal(String str) {
-        double binary = 0;
+        double decimal = 0;
         Integer bit;
-        Integer baseValue = Integer.parseInt (str);
+        Integer originalDecimal = Integer.parseInt (str);
         Integer bitPadding = Integer.parseInt (str);
-        StringBuffer stringBuffer = new StringBuffer ();
-        String tempStr;
+        StringBuffer originalBinary = new StringBuffer ();
+        String highestPower;
 
         //Decimal to binary
-        while (baseValue != 0){
-            bit = baseValue%2;
-            baseValue = baseValue/2;
-            stringBuffer.append (bit);
+        while (originalDecimal != 0){
+            bit = originalDecimal%2;
+            originalDecimal = originalDecimal/2;
+            originalBinary.append (bit);
         }
 
         //padding 8 bites
         if (bitPadding <= 255){
-            for (int i = stringBuffer.length () ; i < 8; i++) {
-                stringBuffer.append (0);
+            for (int i = originalBinary.length () ; i < 8; i++) {
+                originalBinary.append (0);
             }
         }
         //padding 16 bites
         if (bitPadding >255 && bitPadding <= 65535){
-            for (int i = stringBuffer.length () ; i < 16; i++) {
-                stringBuffer.append (0);
+            for (int i = originalBinary.length () ; i < 16; i++) {
+                originalBinary.append (0);
             }
         }
         // for future expansion
@@ -36,22 +36,15 @@ public class BinaryReversal {
         //padding 64 bites
 
         //Binary to decimal
-        tempStr = stringBuffer.toString ();
-        for (int i = 0; i < tempStr.length (); i++) {
-            bit = Integer.parseInt (tempStr.substring (i,i+1));
-            binary += ((bit * Math.pow (2, ((tempStr.length ()-1) - i))));
+        highestPower = originalBinary.toString ();//inorder to the highest power
+        for (int i = 0; i < highestPower.length (); i++) {
+            bit = Integer.parseInt (highestPower.substring (i,i+1));
+            decimal += ((bit * Math.pow (2, ((highestPower.length ()-1) - i))));
         }
 
-        int value = (int)binary;//will change 244.0 -> 244
-        String myS = String.valueOf (value);//changing to fit return compatibility
+        int value = (int)decimal;//will change 244.0 -> 244
+        String reverseDecimal = String.valueOf (value);//changing to fit return compatibility
 
-        return myS;
+        return reverseDecimal;
     }
-    //Have the function BinaryReversal(str) take the str parameter being passed,
-//    which will be a positive integer, take its binary representation (padded to the nearest N * 8 bits)
-//    , reverse that string of bits, and then finally return the new reversed string in decimal form.
-//    For example: if str is &quot;47&quot; then the binary version of this integer is 101111
-//    but we pad it to be 00101111. Your program should reverse this binary
-//        string which then becomes: 11110100 and then finally return the decimal version of this string,
-//        which is 244.
 }
